e0f2601aa18c32d8b4314aa769519bb27f16abf2
Gundars Vaivads <gundars.vaivads@gmail.com>
2022-10-07 10:24:26 +0300

    updated

--- uzdevums.py
+++ uzdevums.py
@@ -1,4 +1,3 @@
-
 import json
 
 file = open('students.json', 'r')
@@ -8,3 +7,22 @@ file.close()
 print(data)
 for student in data['Students']:
         print(f"{student['FirstName']} {student['LastName']} {student['Age']}")
+
+
+
+total_age = 0
+count = 0
+for student in data['Students']:
+    total_age += int(student['Age'])
+    count += 1
+
+print('===================================')
+print(f'Average age is {total_age / count}')
+
+
+
+ages = []
+for student in data['Students']:
+    ages.append(int(student['Age']))
+
+print(sum(ages) / len(ages))
